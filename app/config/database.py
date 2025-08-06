6c93ca9aec2c2a70a3ba0c0b86e48c10160994a9
Mariem Abida <158767911+Abida610@users.noreply.github.com>
2025-08-06 13:25:08 +0100

    Update database.py

--- app/config/database.py
+++ app/config/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 
-engine = create_engine( "postgresql+psycopg2://postgres:maryem222@192.168.208.1:5432/atm_app?options=-c%20client_encoding%3Dutf8"
+engine = create_engine( ""
 )
 SessionLocal = sessionmaker( bind=engine,autocommit=False, autoflush=False)
 Base = declarative_base()
